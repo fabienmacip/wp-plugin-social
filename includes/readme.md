81a42505e76c209745843138b866b0a7a1c0bfb1
fabienmacip <fabien.macip@gmail.com>
2022-04-12 21:11:39 +0200

    interface admin ok

---
+++ includes/readme.md
@@ -0,0 +1,10 @@
+# Installer le plugin file manager
+
+# Config
+Dans wp-config.php, mettre WP_DEBUG à true.  
+
+# icone du plugin
+developer.wordpress.org/resource/dashicons/#menu  
+
+Copier le CSS de l'icône du plugin, directement dans le CSS additionnel du thème
+
--- includes/reseaux-sociaux-functions.php
+++ includes/reseaux-sociaux-functions.php
@@ -40,3 +40,96 @@ function social_media_topbar($wp_admin_bar){
 add_action('admin_bar_menu','social_media_topbar', 999);
 
 /* END - ADMIN TOPBAR */
+
+
+/* START - CREATION DE LA PAGE ADMIN : FONCTION */
+
+function social_media_section_settings(){
+  // Création d'une section
+  add_settings_section("social_media_section", "", null, "social-media");
+
+  // Création des champs
+  add_settings_field("social-media-facebook", "Partager sur Facebook", "social_media_facebook_switch", "social-media", "social_media_section");
+  add_settings_field("social-media-whatsapp", "Partager sur What's app", "social_media_whatsapp_switch", "social-media", "social_media_section");
+  add_settings_field("social-media-linkedin", "Partager sur Linked In", "social_media_linkedin_switch", "social-media", "social_media_section");
+  add_settings_field("social-media-twitter", "Partager sur Twitter", "social_media_twitter_switch", "social-media", "social_media_section");
+
+  // Enregistrement des champs
+  register_setting("social_media_section", "social-media-facebook");
+  register_setting("social_media_section", "social-media-whatsapp");
+  register_setting("social_media_section", "social-media-linkedin");
+  register_setting("social_media_section", "social-media-twitter");
+
+}
+
+/* END - CREATION DE LA PAGE ADMIN : FONCTION */
+
+
+/* START - CREATION DE LA PAGE ADMIN : HTML */
+
+function social_media_facebook_switch(){ ?>
+  <label class="switch">
+    <input type="checkbox" name="social-media-facebook" value="activate" <?php checked('activate', get_option('social-media-facebook'), true); ?> />
+    <span class="slider round"></span>
+  </label>
+<?php } 
+
+function social_media_whatsapp_switch(){ ?>
+  <label class="switch">
+    <input type="checkbox" name="social-media-whatsapp" value="activate" <?php checked('activate', get_option('social-media-whatsapp'), true); ?> />
+    <span class="slider round"></span>
+  </label>
+<?php } 
+
+function social_media_linkedin_switch(){ ?>
+  <label class="switch">
+    <input type="checkbox" name="social-media-linkedin" value="activate" <?php checked('activate', get_option('social-media-linkedin'), true); ?> />
+    <span class="slider round"></span>
+  </label>
+<?php } 
+
+function social_media_twitter_switch(){ ?>
+  <label class="switch">
+    <input type="checkbox" name="social-media-twitter" value="activate" <?php checked('activate', get_option('social-media-twitter'), true); ?> />
+    <span class="slider round"></span>
+  </label>
+<?php } 
+
+add_action("admin_init", "social_media_section_settings");
+
+/* END - CREATION DE LA PAGE ADMIN : HTML */
+
+
+/* START - CREATION DE LA PAGE ADMIN : FORMULAIRE HTML */
+
+function social_media_page(){ ?>
+
+  <div class="container">
+    <h1>Réseaux sociaux</h1>
+
+    <form method="post" action="options.php">
+      <?php
+        settings_fields("social_media_section");
+        do_settings_sections("social-media");
+        submit_button();
+      ?>
+    </form>
+  </div>
+
+<?php }
+
+
+
+
+/* END - CREATION DE LA PAGE ADMIN : FORMULAIRE HTML */
+
+
+
+
+
+
+
+
+
+
+
